6a34bbf8d59f1e6df3c6f1e871e052e5967e3055
CuongNC <cuongnc@gonrin.com>
2019-10-28 19:58:03 +0700

    update requirement text

--- application/config.py
+++ application/config.py
@@ -2,7 +2,6 @@ class Config(object):
     DEBUG = True
     STATIC_URL = "static"
     SQLALCHEMY_DATABASE_URI = 'sqlite:///database.db'
-    #SQLALCHEMY_DATABASE_URI = 'postgresql://cuongnc:123456@192.168.125.132:5432/gatcotest'
     AUTH_LOGIN_ENDPOINT = 'login'
     AUTH_PASSWORD_HASH = 'sha512_crypt'
     AUTH_PASSWORD_SALT = 'add_salt'
