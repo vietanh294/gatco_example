0f96a951a9ed92f6e595fb4257f16190839ab6ef
CuongNC <cuongnc@gonrin.com>
2019-11-23 06:17:04 +0700

    update model

--- application/controllers/__init__.py
+++ application/controllers/__init__.py
@@ -1,14 +1,11 @@
 # Register Blueprints/Views.
-from .users import UserController
-from .upload import upload
 from application.extensions import jinja
 
 def init_views(app):
-    app.add_route(UserController.as_view(), '/api/user')
-    import application.controllers.pages
+    import application.controllers.user
     import application.controllers.api
-    
-    app.blueprint(upload)
+    # app.blueprint(upload)
+
     @app.route('/')
     def index(request):
         return jinja.render('index.html', request)
--- application/controllers/api.py
+++ application/controllers/api.py
@@ -1,5 +1,5 @@
 from application.extensions import apimanager
-from application.models.users import User, QuocGia, TinhThanh, TinhThanh2
+from application.models.model import User, QuocGia, TinhThanh
 from application.extensions import auth
 from gatco.exceptions import ServerError
 from gatco_restapi import  ProcessingException
@@ -18,33 +18,14 @@ apimanager.create_api(User,
     collection_name='user',
     include_columns=['id', 'birthday', 'email','is_active'])
 
-apimanager.create_api(QuocGia,
+apimanager.create_api(collection_name='quocgia', Model=QuocGia,
     methods=['GET', 'POST', 'DELETE', 'PUT'],
     url_prefix='/api/v1',
     preprocess=dict(GET_SINGLE=[auth_func], GET_MANY=[auth_func], POST=[auth_func], PUT_SINGLE=[auth_func]),
-    collection_name='quocgia')
+    )
 
-apimanager.create_api(TinhThanh,
+apimanager.create_api(collection_name='tinhthanh', Model=TinhThanh,
     methods=['GET', 'POST', 'DELETE', 'PUT'],
     url_prefix='/api/v1',
     preprocess=dict(GET_SINGLE=[auth_func], GET_MANY=[auth_func], POST=[auth_func], PUT_SINGLE=[auth_func]),
-    collection_name='tinhthanh')
-
-
-apimanager.create_api(TinhThanh2,
-    methods=['GET', 'POST', 'DELETE', 'PUT'],
-    url_prefix='/api/v1',
-    preprocess=dict(GET_SINGLE=[auth_func], GET_MANY=[auth_func], POST=[auth_func], PUT_SINGLE=[auth_func]),
-    collection_name='tinhthanh2')
-# 
-# apimanager.create_api(Parent,
-#     methods=['GET', 'POST', 'DELETE', 'PUT'],
-#     url_prefix='/api/v1',
-#     preprocess=dict(GET_SINGLE=[auth_func], GET_MANY=[auth_func], POST=[auth_func], PUT_SINGLE=[auth_func]),
-#     collection_name='parent')
-# 
-# apimanager.create_api(Child2,
-#     methods=['GET', 'POST', 'DELETE', 'PUT'],
-#     url_prefix='/api/v1',
-#     preprocess=dict(GET_SINGLE=[auth_func], GET_MANY=[auth_func], POST=[auth_func], PUT_SINGLE=[auth_func]),
-#     collection_name='child2')
+    )
